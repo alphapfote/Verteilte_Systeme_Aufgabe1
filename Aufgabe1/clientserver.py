7869d795c970d6307edcd8ad78342be3983bea85
Jana Becker <jbecker@inovex.de>
2018-11-05 15:54:40 +0100

    Initial project

--- Aufgabe1/clientserver.py
+++ Aufgabe1/clientserver.py
@@ -1,5 +1,6 @@
 import logging
 import socket
+import json
 
 import constCS
 import lab_logging
@@ -10,23 +11,53 @@ lab_logging.setup()  # init loging channels for the lab
 class Server:
     logger = logging.getLogger("vs2lab.a1_layers.clientserver.Server")
 
+    tel = {'natalia': '1234', 'alice': '8765', 'jana': '2921', 'hendrik': '1801'}
+
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((constCS.HOST, constCS.PORT))
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # prevents errors due to "addresses in use"
+        self.sock.bind((constCS.HOST, constCS.PORT))
         self.logger.info("Server bound to socket " + str(self.sock))
 
     def serve(self):
         self.sock.listen(1)
+        self.logger.info("Server is listening for client")
+
         (connection, address) = self.sock.accept()  # returns new socket and address of client
+        
         while True:  # forever
-            data = connection.recv(1024)  # receive data from client
+            data = connection.recv(1024).decode('ascii')  # receive data from client
+
+            # print ("Erhaltener command:" + data.decode('ascii'))
             if not data:
-                break  # stop if client stopped
-            connection.send(data + "*".encode('ascii'))  # return sent data plus an "*"
+                # TCP-Connection lost
+                self.logger.info("Server didn´t get data")
+                break
+
+            self.logger.info("Server got some data: " + data)            
+            # Send all
+            if data == 'getAll':
+                teljson = json.dumps(self.tel)
+                #for key, value in self.tel.items():
+                print(teljson)
+                connection.send(teljson.encode('ascii'))
+                self.logger.info("Server sent data: " + teljson)
+                break
+
+            # Send single name 
+            if data in self.tel:   
+                connection.send((data + ' - ' + self.tel[data]).encode('ascii'))
+                self.logger.info("Server sent data: " + self.tel[data])
+                break
+
+            # Send data not found
+            else:
+                connection.send(('No data found for ' + data).encode('ascii'))
+                break
+            
         connection.close()  # close the connection
         self.sock.close()
-        self.logger.info("Server down.")
+        self.logger.info("Server closed connection.")
 
 
 class Client:
@@ -45,3 +76,30 @@ class Client:
         self.sock.close()  # close the connection
         self.logger.info("Client down.")
         return msg_out
+
+    def get(self, name):
+        self.sock.send(name.encode('ascii')) # send name as string
+        self.logger.info("Client sent name: " + name)
+
+        data = self.sock.recv(1024)  # receive the response
+        self.logger.info("Client got response: " + data.decode('ascii'))
+
+        number = data.decode('ascii')
+        self.sock.close()  # close the connection
+        self.logger.info("Client closed connection.")
+        return number
+
+    def getAll(self):
+        self.sock.send('getAll'.encode('ascii')) # send name as string
+        self.logger.info("Client sent getAll")
+
+        data = self.sock.recv(1024)  # receive the respone
+        output = data.decode('ascii')
+        self.logger.info("Client got response: " + output)
+
+        dataToArray = json.loads(data)
+        for key, value in dataToArray.items():
+           print(key + ' ' + value + ';')
+        self.sock.close()  # close the connection
+        self.logger.info("Client closed connection.")
+        return dataToArray
--- Aufgabe1/clientserver_test.py
+++ Aufgabe1/clientserver_test.py
@@ -1,10 +1,12 @@
 import os
 import unittest
+import time
 
 import clientserver
 
 class TestEchoService(unittest.TestCase):
     def setUp(self):
+        time.sleep(3)
         super().setUp()
         server = clientserver.Server()
         pid = os.fork()
@@ -14,9 +16,16 @@ class TestEchoService(unittest.TestCase):
         self.client = clientserver.Client()
 
     def test_srv_get(self):
-        msg = self.client.call("Hello VS2Lab")
-        self.assertEqual(msg, 'Hello VS2Lab*')
+        msg = self.client.get('alice')
+        self.assertEqual(msg, 'alice - 8765')
 
+    def test_srv_getAll(self):
+        msg = self.client.getAll()
+        self.assertEqual(msg, {'natalia': '1234', 'alice': '8765', 'jana': '2921', 'hendrik': '1801'})
+
+    def test_srv_get_wrong_name(self):
+        msg = self.client.get('bob')
+        self.assertEqual(msg, 'No data found for bob')
 
 if __name__ == '__main__':
     unittest.main()
